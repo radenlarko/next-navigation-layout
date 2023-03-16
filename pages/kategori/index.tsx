6c1cf684cf2fe67012e0b078301fde1303e96378
radenlarko <radenlarko@gmail.com>
2023-03-16 14:55:32 +0700

    Update UI kategori list

--- pages/kategori/index.tsx
+++ pages/kategori/index.tsx
@@ -1,13 +1,12 @@
 import Link from "next/link";
-import { KategoriItem, LinkItem } from "@/types";
+import { KategoriItem } from "@/types";
 import { menuDesktop } from "@/utils/listMenu";
-import { reduceLinkItem } from "@/utils/myFunction";
 import { Box, Flex, SimpleGrid, Text } from "@chakra-ui/react";
 import { GetStaticProps, NextPage } from "next";
 import React from "react";
 
 interface Props {
-  data: LinkItem[];
+  data: KategoriItem[];
 }
 
 const Kategori: NextPage<Props> = ({ data }) => {
@@ -16,25 +15,37 @@ const Kategori: NextPage<Props> = ({ data }) => {
       <Text fontWeight={500} fontSize="2xl">
         Kategori List
       </Text>
-      <SimpleGrid columns={3} gap={4} mt={8}>
-        {data.map((item) => (
-          <Flex
-            key={item.link}
-            as={Link}
-            bg="gray.200"
-            alignItems="center"
-            justifyContent="center"
-            p={2}
-            borderRadius="lg"
-            _hover={{ bg: "pink.100" }}
-            href={`/kategori/${item.name}`}
-          >
-            <Text textAlign="center" fontSize="sm">
-              {item.name}
-            </Text>
-          </Flex>
-        ))}
-      </SimpleGrid>
+      {data.map((item) => (
+        <Box key={item.name} mt={8}>
+          <Text fontWeight={500} fontSize="xl">
+            {item.name}
+          </Text>
+          {item.items.map((child) => (
+            <Box key={child.title} mt={2}>
+              <Text textDecoration="underline">{child.title}</Text>
+              <SimpleGrid columns={3} gap={4} mt={1}>
+                {child.links.map((link) => (
+                  <Flex
+                    key={link.link}
+                    as={Link}
+                    bg="gray.200"
+                    alignItems="center"
+                    justifyContent="center"
+                    p={2}
+                    borderRadius="lg"
+                    _hover={{ bg: "pink.100" }}
+                    href={`/kategori/${link.name}`}
+                  >
+                    <Text textAlign="center" fontSize="sm">
+                      {link.name}
+                    </Text>
+                  </Flex>
+                ))}
+              </SimpleGrid>
+            </Box>
+          ))}
+        </Box>
+      ))}
     </Box>
   );
 };
@@ -43,7 +54,7 @@ export default Kategori;
 
 export const getStaticProps: GetStaticProps<Props> = async () => {
   const idx = menuDesktop.findIndex((val) => val.name === "Kategori");
-  const mainData = reduceLinkItem(menuDesktop[idx].items as KategoriItem[]);
+  const mainData = menuDesktop[idx].items as KategoriItem[];
   return {
     props: {
       data: mainData,
